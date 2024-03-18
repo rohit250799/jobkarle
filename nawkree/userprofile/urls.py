efaf819914d556ca5c38b85886e92d4ce70f7c1f
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-03-18 22:32:35 +0530

    Adding url paths for dashboard and viewing the job application details

---
+++ nawkree/userprofile/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path, include
+from .views import dashboard, view_application
+
+urlpatterns = [
+    path('', dashboard, name='dashboard'),
+    path('application/<int:application_id>/', view_application, name='view_application'),
+]
