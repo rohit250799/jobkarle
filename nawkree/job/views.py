80722082ea593c84ad723d3b364e294653ccf70a
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-03-18 22:12:00 +0530

    Creating methods for applying to jobs and for adding jobs

--- nawkree/job/views.py
+++ nawkree/job/views.py
@@ -1,9 +1,49 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from .models import Job
+from .forms import AddJobForm, ApplicationForm
 # Create your views here.
 
 def job_detail(request, job_id):
     job = Job.objects.get(pk=job_id)
     return render(request, 'job/job_detail.html', {
         'job': job
+    })
+
+@login_required
+def apply_for_job(request, job_id):
+    job = Job.objects.get(pk=job_id)
+    if request.method == 'POST':
+        form = ApplicationForm(request.POST)
+        if form.is_valid():
+            application = form.save(commit=False)
+            application.job = job
+            application.created_by = request.user
+            application.save()
+
+            return redirect('dashboard')
+    else:
+        form = ApplicationForm()
+    
+    return render(request, 'job/apply_for_job.html', {
+        'form': form,
+        'job': job,
+    }) 
+
+@login_required
+def add_job(request):
+    if request.method == 'POST':
+        form = AddJobForm(request.POST)
+
+        if form.is_valid():
+            job = form.save(commit=False)
+            job.created_by = request.user
+            job.save()
+
+            return redirect('dashboard')
+    else:
+        form = AddJobForm()
+    
+    return render(request, 'job/add_job.html', {
+        'form': form,
     })
