4be7a9a7a10e01bb8dae6f8895d7743b88385f18
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-03-18 22:09:39 +0530

    Changing path url for job_detail, adding url paths for adding jobs and applying for job

--- nawkree/job/urls.py
+++ nawkree/job/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . import views
+from . views import job_detail, add_job, apply_for_job
 
 urlpatterns = [
-    path('jobs/<int:job_id>', views.job_detail, name='job_detail'),
+    path('<int:job_id>/', job_detail, name='job_detail'),
+    path('add/', add_job, name='add_job'),
+    path('<int:job_id>/apply_for_job/', apply_for_job, name='apply_for_job' ), 
 ]
