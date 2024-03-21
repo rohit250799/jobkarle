e0b431729311233f7c5439fc7ff958cfdcf97e90
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-03-21 08:29:11 +0530

    Updating the view application method and creating a view dashboard job method

--- nawkree/userprofile/views.py
+++ nawkree/userprofile/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from job.models import Application
+from job.models import Application, Job
+from .models import ConversationMessage
 
 # Create your views here.
 @login_required
@@ -11,7 +12,24 @@ def dashboard(request):
 
 @login_required
 def view_application(request, application_id):
-    application = get_object_or_404(Application, pk=application_id, created_by = request.user)
+    if request.user.userprofile.is_employer:
+        application = get_object_or_404(Application, pk=application_id, job__created_by = request.user)
+    else:
+        application = get_object_or_404(Application, pk=application_id, created_by = request.user)
+
+    if request.method == 'POST':
+        content = request.POST.get('content')
+        if content:
+            conversationmessage = ConversationMessage.objects.create(application=application, content=content, created_by=request.user)
+            return redirect('view_application', application_id=application_id)
+    
     return render(request, 'userprofile/view_application.html', {
         'application': application
+    })
+
+@login_required
+def view_dashboard_job(request, job_id):
+    job = get_object_or_404(Job, pk=job_id, created_by=request.user)
+    return render(request, 'userprofile/view_dashboard_job.html', {
+        'job': job
     })
