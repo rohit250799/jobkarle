95589b2ff7de267ec180d2221a1a39217b3ec16d
Rohit Sarkar <rohitsarkar034@gmail.com>
2024-03-24 09:11:35 +0530

    Creating a edit job function and adding its url path

--- nawkree/job/urls.py
+++ nawkree/job/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from . views import job_detail, add_job, apply_for_job, search
+from . views import job_detail, add_job, apply_for_job, search, edit_job
 from .api import api_search
 
 urlpatterns = [
     path('api/search/', api_search, name='api_search'),
     path('search/', search, name='search'),
     path('<int:job_id>/', job_detail, name='job_detail'),
+    path('<int:job_id>/edit/', edit_job, name='edit_job'),
     path('add/', add_job, name='add_job'),
     path('<int:job_id>/apply_for_job/', apply_for_job, name='apply_for_job' ), 
 ]
